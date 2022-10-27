f0b024b225b71b7cc27f2097924361a597c99cf0
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2022-10-26 22:39:03 -0700

    React-to-pdf is implemented

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
-import { FaStar, FaStarHalfAlt, FaDownload } from 'react-icons/fa';
+import { FaStar, FaStarHalfAlt, FaDownload, FaAddressCard } from 'react-icons/fa';
 import { FcRating } from 'react-icons/fc';
 import { MdReviews, MdOutlineWork } from 'react-icons/md';
 import './Details.css'
+import Pdf from "react-to-pdf";
+const ref = React.createRef();
 
 const Details = () => {
+
     const data = useLoaderData();
 
     const { id, image, contents, language, price, rating, short_description, title, tutor, enrolled } = data;
@@ -15,74 +18,81 @@ const Details = () => {
 
     const { Course_content, description, learn, requirements
     } = contents;
-    console.log(data);
+
+
+
+
     return (
         <div className='mt-5'>
             <div className='d-flex justify-content-between'>
                 <h3>{title}</h3>
-                <FaDownload />
+                <Pdf targetRef={ref} filename={`${title}-course-details.pdf`} >
+                    {({ toPdf }) => <FaDownload className='download-btn' onClick={toPdf} />}
+                </Pdf>
             </div>
+
 
             <img src={image} className="rounded img-fluid my-3" alt="img" />
 
-            <p>{short_description}</p>
+            <div ref={ref}>
+                <h1>{title}</h1>
+                <p>{short_description}</p>
 
-            <div className='d-flex justify-content-between'>
-                <div className='d-flex flex-column '>
-                    <p className='fw-bold m-0'>
-                        {rating}
-                    </p>
-                    <div >
-                        <FaStar />
-                        <FaStar />
-                        <FaStar />
-                        <FaStar />
-                        <FaStarHalfAlt />
+                <div className='d-flex justify-content-between'>
+                    <div className='d-flex flex-column '>
+                        <p className='fw-bold m-0'>
+                            {rating}
+                        </p>
+                        <div >
+                            <FaStar />
+                            <FaStar />
+                            <FaStar />
+                            <FaStar />
+                            <FaStarHalfAlt />
+                        </div>
+                    </div>
+
+                    <div>
+                        <p> <span className='fw-bold'>Enrolled:</span> {enrolled} students</p>
+                    </div>
+                </div>
+
+                <div className='mt-4'>
+                    <p><span className='fw-bold'>Mentor:</span> {name}</p>
+                </div>
+
+                <hr />
+
+                <div className='mb-4'>
+                    <h3>Descriptions</h3>
+
+                    <div>
+                        {
+                            description
+                        }
                     </div>
                 </div>
 
                 <div>
-                    <p> <span className='fw-bold'>Enrolled:</span> {enrolled} students</p>
+                    <h3>Outcomes</h3>
+
+                    <div>
+                        {
+                            learn.map(topic => <li>{topic}</li>)
+                        }
+                    </div>
+                </div>
+
+                <div className='mt-5'>
+                    <h4>Course Contents</h4>
+
+                    <ul className='list-group mt-3'>
+                        {
+                            Course_content.map(content => <li className='list-group-item'>{content}</li>)
+                        }
+                    </ul>
                 </div>
             </div>
-
-            <div className='mt-4'>
-                <p><span className='fw-bold'>Mentor:</span> {name}</p>
-            </div>
-
-            <hr />
-
-            <div className='mb-4'>
-                <h3>Descriptions</h3>
-
-                <div>
-                    {
-                        description
-                    }
-                </div>
-            </div>
-
-            <div>
-                <h3>Outcomes</h3>
-
-                <div>
-                    {
-                        learn.map(topic => <li>{topic}</li>)
-                    }
-                </div>
-            </div>
-
-
-            <div className='mt-5'>
-                <h4>Course Contents</h4>
-
-                <ul className='list-group mt-3'>
-                    {
-                        Course_content.map(content => <li className='list-group-item'>{content}</li>)
-                    }
-                </ul>
-            </div>
-
             <div className='mt-4'>
                 <h3>Requirements</h3>
                 <div className='mt-4'>
@@ -107,6 +117,7 @@ const Details = () => {
                     <p><FcRating /> {tutor.rating}</p>
                     <p><MdReviews /> {reviews} reviews</p>
                     <p><MdOutlineWork /> {profession} </p>
+                    <p><FaAddressCard />{location}</p>
                 </div>
             </div>
 
@@ -118,6 +129,7 @@ const Details = () => {
             <div>
                 <Link to={`/courses/cart/course/${id}`}><button className='premium-btn my-3'>Get premium access</button></Link>
             </div>
+
         </div>
     );
 };
